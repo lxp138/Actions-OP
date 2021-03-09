a66efc5051dd2a956e73d6e869546c19b763fcf8
lxp138 <76868309+lxp138@users.noreply.github.com>
2021-03-09 23:08:03 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -5,8 +5,4 @@
 # Author: digitcloud
 #=================================================
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.50.2/g' package/base-files/files/bin/config_generate
-
-# Delete default password
-sed -i "/CYXluq4wUazHjmCDBCqXF/d" package/default-settings/files/zzz-default-settings
-
+sed -i 's/192.168.1.1/192.168.100.3/g' package/base-files/files/bin/config_generate
